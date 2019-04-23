56f14e34c5fbafc2364fcf82c3bc4803a69a40c2
NightOwl <lhh80482@gmail.com>
2019-04-23 14:47:13 +0900

    Init Commit

---
+++ src/BookManagement.java
@@ -0,0 +1,22 @@
+
+
+
+public class BookManagement {
+	
+	private int bookCount = 0;
+	
+	public static void main(String[] args) {
+		System.out.println("Hello!!!!!!!!!!!!!!!");
+	}
+	
+	public boolean addBook(int bookNumber) {
+		if(bookNumber>0) {
+			bookCount ++;
+			return true;
+		}
+		return false;
+	}
+	public int getBookCount() {
+		return bookCount;
+	}
+}
---
+++ src/BookManagementTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookManagementTest {
+
+	@Test
+	public void testAddBook() {
+		BookManagement book_management = new BookManagement();
+		assertTrue(book_management.addBook(2));
+	}
+	@Test
+	public void testAddBook_False() {
+		BookManagement book_management = new BookManagement();
+		assertFalse(book_management.addBook(-1));
+	}
+
+	@Test
+	public void testGetBookCount() {
+		BookManagement book_management = new BookManagement();
+		assertTrue(book_management.getBookCount()==0);
+	}
+}
